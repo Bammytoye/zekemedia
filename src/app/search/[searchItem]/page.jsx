046ef38db01189d90dcff7281326f031e2f93234
bammytoye <bamigbalatoyese@gmail.com>
2024-08-31 20:10:40 +0100

    search

--- src/app/search/[searchItem]/page.jsx
+++ src/app/search/[searchItem]/page.jsx
@@ -2,9 +2,9 @@ import MoviesResults from '@/components/MoviesResults';
 import React from 'react'
 
 export default async function searchPage({ params}) {
-    const searchPage = params.searchItem
-    const res = await fetch (
-        `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchPage}&
+    const searchItem = params.searchItem
+    const res = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchItem}&
         language=en-US&page=1&include_adult=false`
     );
     
@@ -14,9 +14,11 @@ export default async function searchPage({ params}) {
     return (
         <div>
             {results && 
-            results.length === 
-            (<h1 className='text-center pt-6'>No results found</h1>)}
-            {results && <MoviesResults result={result} />}
-    </div>
+            results.length === 0 (
+                    <h1 className='text-center pt-6'>No results found</h1>
+                )
+            }
+            {results && <MoviesResults results={results} />}
+    </div> 
     )
 }
