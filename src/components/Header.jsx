86bdb80fb261506bbf16d01f0e815d259a95fb0b
bammytoye <bamigbalatoyese@gmail.com>
2024-09-06 12:14:45 +0100

    finalPart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import MenuBar from './MenuBar'
+import React from 'react';
+import MenuBar from './MenuBar';
 import { TbHomeMove } from "react-icons/tb";
 import { IoInformationCircle } from "react-icons/io5";
 import Link from 'next/link';
 import DarkModeSwitch from './DarkModeSwitch';
 
-
 export default function Header() {
     return (
-        <div className='flex justify-between items-center px-7 py-5 max-w-6xl mx-auto dark:text-white'>
-            <div className="flex space-x-5">
-                <MenuBar title="Home" address="/" Icon={TbHomeMove} />
-                <MenuBar title="About" address="/about" Icon={IoInformationCircle}/>
-            </div>
+        <div className="fixed top-0 left-0 right-0 bg-white dark:bg-gray-600 shadow-md z-50">
+            <div className='flex justify-between items-center px-7 py-5 max-w-6xl mx-auto dark:text-white'>
+                <div className="flex space-x-5">
+                    <Link href={'/'} className='flex gap-1 items-center cursor-pointer'>
+                        <span className='text-sm bg-amber-500 hover:text-white dark:hover:text-gray-700 py-1 px-2 rounded-lg font-extrabold'>Zeke</span>
+                        <span className='text-sm hidden sm:inline font-extrabold hover:text-amber-500 dark:text-amber-500 dark:hover:text-white hover:border-b-2 dark:hover:border-white hover:border-amber-500'>Media</span>
+                    </Link>
+                    <MenuBar title="Home" address="/" Icon={TbHomeMove} />
+                    <MenuBar title="About" address="/about" Icon={IoInformationCircle} />
+                </div>
 
-            <div className='flex gap-4 items-center'>
-                <DarkModeSwitch />
-                <Link href={'/'} className='flex gap-1 items-center cursor-pointer'>
-                    <span className='text-sm bg-amber-500 hover:text-white dark:hover:text-gray-700 py-1 px-2 rounded-lg font-extrabold'>Zeke</span>
-                    <span className='text-sm hidden sm:inline font-extrabold hover:text-amber-500 dark:text-amber-500 dark:hover:text-white hover:border-b-2 dark:hover:border-white hover:border-amber-500'>Media</span>
-                </Link>
+                <div className='flex gap-4 items-center'>
+                    <div>
+                        <DarkModeSwitch />
+                    </div>
+                </div>
             </div>
         </div>
-    )
+    );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import NavbarItem from './NavbarItem'
 
 export default function Navbar() {
     return (
-        <div className='flex gap-8 justify-center items-center bg-amber-200 dark:bg-gray-300 py-2 cursor-pointer'>
+        <div className='flex mt-[67px] gap-8 justify-center items-center bg-amber-200 dark:bg-gray-300 py-2 cursor-pointer'>
             <NavbarItem title='Trending' param='fetchTrending'/>
             <NavbarItem title='Top Rated' param='fetchTopRated'/>
         </div>
